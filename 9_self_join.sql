16b6f596ca3a6efe4899d6e9a7e14f45a2b9c964
Aaryamann171 <aaryamann171@gmail.com>
2020-12-06 22:43:04 +0530

    excercise 9 completed

--- 9_self_join.sql
+++ 9_self_join.sql
@@ -34,3 +34,44 @@ HAVING COUNT(*) = 2
 SELECT a.company, a.num, a.stop, b.stop 
 FROM route AS a JOIN route AS b ON (a.company = b.company AND a.num = b.num)
 WHERE a.stop = 53 AND b.stop = 149
+
+-- 6
+/* The query shown is similar to the previous one, however by joining two copies of the stops table we can refer to stops by name rather than by number. Change the query so that the services between 'Craiglockhart' and 'London Road' are shown. If you are tired of these places try 'Fairmilehead' against 'Tollcross' */
+
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON (a.company=b.company AND a.num=b.num)
+JOIN stops stopa ON (a.stop=stopa.id)
+JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' and stopb.name = 'London Road'
+
+-- 7
+/* Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith') */
+
+SELECT DISTINCT a.company, a.num
+FROM route a
+JOIN route b ON a.num = b.num
+WHERE a.stop = 115
+AND b.stop = 137
+
+-- 8
+/* Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross' */
+
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON (a.num = b.num)
+JOIN stops stopa ON a.stop = stopa.id
+JOIN stops stopb ON b.stop = stopb.id
+WHERE stopa.name = 'Craiglockhart'
+AND stopb.name = 'Tollcross'
+
+-- 9
+/* Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. Include the company and bus no. of the relevant services. */
+
+SELECT DISTINCT stopb.name, b.company, b.num
+FROM route a
+JOIN route b ON (a.num = b.num AND a.company = b.company)
+JOIN stops stopa ON (a.stop = stopa.id)
+JOIN stops stopb ON (b.stop = stopb.id)
+WHERE stopa.name = 'Craiglockhart'
+
+-- 10
+/* Find the routes involving two buses that can go from Craiglockhart to Lochend. Show the bus no. and company for the first bus, the name of the stop for the transfer, and the bus no. and company for the second bus. */
